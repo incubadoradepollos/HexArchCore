75ddadd92bbe553ac4a65062aa01a72bda505a92
Incubadora de Pollos <incubadoradepollos@gmail.com>
2025-03-30 10:28:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hexarch_core",
-    version="0.1.2",
+    version="0.0.1",
     author="Jose Manuel Herera",
     author_email="jmherrera76@gmail.com",
     description="Core for hexarch_core",
